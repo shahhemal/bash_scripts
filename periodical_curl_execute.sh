89e9ee6dde63d790b7c0180f043136af12b7296e
Hemal Shah <hshah@intralinks.com>
2018-03-20 15:49:41 -0400

    adding query

--- periodical_curl_execute.sh
+++ periodical_curl_execute.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Periodically executes curl command and captures response time in a log file
+# curl -u elastic:changeme -XPOST http://localhost:9200/il-search-ilp/_search -d @/home/kbolineni/hubSearchQuery.json
+
 
 LOG_FILE=response_output_$(date +"%Y%m%d%H%M").txt
 echo Logs captured in $(pwd)/$LOG_FILE
@@ -11,7 +13,8 @@ MAX_INTERVAL=10
 
 #while true; do
 while [ $CNT -lt $MAX_REQ ]; do
-    curl -u elastic:changeme -o /dev/null -s -w "Time: $(date)  Total time: %{time_total}\n" localhost:9200 >> $LOG_FILE
+    #curl -u elastic:changeme -o /dev/null -s -w "Time: $(date)  Total time: %{time_total}\n" localhost:9200 >> $LOG_FILE
+    curl -u elastic:changeme -o /dev/null -s -w "Time: $(date)  Total time: %{time_total}\n" -XPOST http://10.34.193.70:9200/il-search-ilp/_search -d @workspaceMetadataQuery_New.json >> $LOG_FILE
 
     CNT=$[$CNT+1]
     #echo $CNT
