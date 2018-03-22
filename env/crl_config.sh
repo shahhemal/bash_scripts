517a830d63d4b7502a1a547cbbc2349c70e49495
Hemal Shah <hshah@intralinks.com>
2018-03-22 18:18:31 -0400

    change config value

--- env/crl_config.sh
+++ env/crl_config.sh
@@ -3,10 +3,10 @@
 # config param file
 
 # Total number of requests to be executed
-MAX_REQUEST=2
+MAX_REQUEST=100
 
 # Max interval between 2 consecutive request eg: 2s (2 secs) or 5m (5 mins)
-MAX_INTERVAL=5s
+MAX_INTERVAL=15m
 
 # Search url
 SEARCH_URL=http://172.25.20.93:9200/il-search-ilp/_search
