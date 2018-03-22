2cb780c0172a9981e47e06ed17a89518a2efac72
Hemal Shah <hshah@intralinks.com>
2018-03-22 17:48:13 -0400

    refactoring

--- env/dev4_config.sh
+++ env/dev4_config.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# commong config param file
+# config param file
+
+# Total number of requests to be executed
+MAX_REQUEST=2
+
+# Max interval between 2 consecutive request eg: 2s (2 secs) or 5m (5 mins)
+MAX_INTERVAL=5s
 
 # Search url
 SEARCH_URL=http://10.34.193.70:9200/il-search-ilp/_search
