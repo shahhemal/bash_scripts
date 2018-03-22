561e7138447cb7343e24c5c1e8beb0bcf34484ea
Hemal Shah <hshah@intralinks.com>
2018-03-22 12:34:04 -0400

    renaming config files

--- crl_common_config.sh
+++ crl_common_config.sh
@@ -7,4 +7,3 @@ SEARCH_URL=http://172.25.20.93:9200/il-search-ilp/_search
 
 # Log file location
 LOG_FILE="/var/log/kibana/response_time_monitoring_test.log"
-#LOG_FILE="logs/response_time_monitoring_test.log"
