7597ac846cafb0a7afb342fcb7cfdb17186e3c2c
Hemal Shah <hshah@intralinks.com>
2018-03-22 14:43:50 -0400

    adding wdl file

---
+++ commons/wdl_common_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# commong config param file
+
+# Search url
+SEARCH_URL=http://172.18.20.46:9200/il-search-ilp/_search
+
+# Log file location
+LOG_FILE="/var/log/kibana/response_time_monitoring_test.log"
